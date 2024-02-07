4bc78eb9777b27b1da9cd05e21f32beec56db378
eviekarimov <evelynk23@nycstudents.net>
2024-02-07 13:59:08 -0500

    upd

--- data.py
+++ data.py
@@ -43,3 +43,14 @@ y=input("give me a sentence")
 print(y)
 y= y.split(" ")
 print(len(y)) 
+
+
+def get_all_factors(n):
+   factors = []
+   for i in range(1,n*1):
+      if n%1 == 0:
+         factors.append(i)
+    return factors
+
+number = int(input("please enter a number: ":))
+
