772b29a3042cae50827145a3e0a298d2b36b2a97
eviekarimov <evelynk23@nycstudents.net>
2024-02-26 13:59:57 -0500

    Update numgame.py

--- numgame.py
+++ numgame.py
@@ -1,15 +1,12 @@
 import random
 number = random.choice (1,2,3,4,5,6,7,8,9,10)
 (x) = int(input("Guess: "))
-if (x) == number:
-    print("correct")
-elif x < number:
-        print("try again; its greater")
-        int(input(input guess again))
-               
-        print("greater")
-elif x > number:
-        print("try again; its smaller")
-        int(input("guess again"))
-    
-  
+while (x) != number:
+    if x > number:
+        print ("It's smellier; try again")
+    if x < number: 
+        print ("Its booger, try again")
+        x = input(guess again 
+else: 
+        print ("you got it!")
+
