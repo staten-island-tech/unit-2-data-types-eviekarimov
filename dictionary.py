3eebeeaaa913f7848ab6c0a91dee1a478cb35af3
eviekarimov <evelynk23@nycstudents.net>
2024-03-05 14:00:51 -0500

    ytes

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+movies = {
+    'title': 'Grown Ups',
+    'genre': 'Comedy',
+    'director': 'Adam Sandler',
+    'released' :
+},{
+     'title': 
+     'genre':
+     'director':
+     'released':
+},{
+    'title': 
+     'genre':
+     'director':
+     'released':
+},{ 
+    'title': 
+     'genre':
+     'director':
+     'released': 
+}
