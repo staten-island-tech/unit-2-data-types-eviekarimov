edb76659ef7dc1eb35772eaa00a973205f97e5e7
eviekarimov <evelynk23@nycstudents.net>
2024-03-12 13:57:57 -0400

    toopl

--- homeroom.py
+++ homeroom.py
@@ -1,3 +1,11 @@
-x = input("last name: ")
-y = x[0]
-def homeroom 
+
+last_name = input("put in last name")
+first_initial = last_name[0].upper()
+print(first_initial)
+
+if first_initial ["A","B","C","D","E","F","G"]: 
+ print("youre in homeroom 101")
+elif first_initial ["H", "I", "J", "K", "L", "M", "N", "O", "P", ] :  
+ print("youre in homeroom 102")
+elif first_initial ["Q", "R", "S", "T", "U" "V", "W", "X", "Y", ]:
+ print("yooure in homeroom 103")
